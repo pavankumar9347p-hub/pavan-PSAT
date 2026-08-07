78cdf9b636bb784a0ef5119dfaa4957a88b25085
pavankumar9347p-hub <pavankumar9347p@gmail.com>
2026-08-07 09:55:28 +0530

    Add files via upload

---
+++ Session61.java
@@ -0,0 +1,22 @@
+Week 2	
+
+Session 6
+1. Build Array from Permutation
+code:
+class Solution {
+    public int[] buildArray(int[] nums) {
+        int n = nums.length;
+        int[] ans = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            ans[i] = nums[nums[i]];
+        }
+
+        return ans;
+    }
+}
+
+
+
+
+
