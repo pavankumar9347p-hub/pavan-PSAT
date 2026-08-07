e4d5534499c4bc1e59887372f8476fb414deaabe
pavankumar9347p-hub <pavankumar9347p@gmail.com>
2026-08-07 09:56:21 +0530

    Add files via upload

--- Session61.java
+++ Session61.java
@@ -1,22 +1,17 @@
-Week 2	
+Week 2
 
-Session 6
-1. Build Array from Permutation
-code:
+
+2. Shuffle the Array 
 class Solution {
-    public int[] buildArray(int[] nums) {
-        int n = nums.length;
-        int[] ans = new int[n];
+    public int[] shuffle(int[] nums, int n) {
+        int[] ans = new int[2 * n];
+        int index = 0;
 
         for (int i = 0; i < n; i++) {
-            ans[i] = nums[nums[i]];
+            ans[index++] = nums[i];
+            ans[index++] = nums[i + n];
         }
 
         return ans;
     }
 }
-
-
-
-
-
